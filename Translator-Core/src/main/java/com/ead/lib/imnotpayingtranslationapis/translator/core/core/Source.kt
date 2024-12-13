8b30dfd7e5e1e05423bcdebe655daf4e5464fe73
darkryh <alex_torres-xc@hotmail.com>
2024-12-12 23:17:00 -0500

    source object

--- Translator-Core/src/main/java/com/ead/lib/imnotpayingtranslationapis/translator/core/core/Source.kt
+++ Translator-Core/src/main/java/com/ead/lib/imnotpayingtranslationapis/translator/core/core/Source.kt
@@ -1,5 +1,6 @@
 package com.ead.lib.imnotpayingtranslationapis.translator.core.core
 
 object Source {
-    const val BASE_URL = "https://translate.google.com"
+    private const val BASE_URL = "https://translate.google.com/?sl="
+    fun getTranslateMode(from: Language, to: Language) = "$BASE_URL${from.code}&tl=${to.code}&op=translate"
 }
